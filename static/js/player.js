4da467ec401d5895df76b0a7b99e22d383d27835
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-27 12:14:59 +0600

    implement simple jPlayer wrapper

---
+++ static/js/player.js
@@ -0,0 +1,42 @@
+var Player = function(params) {
+   var container = document.getElementById(params.id);
+   if (container == undefined) {
+      console.error('Failed to init player');
+      return;
+   }
+
+   var engine = 'flash,html';
+   try {
+      if (browser.getFlashVersion() < 10)
+         engine = 'html,flash';
+   } catch(e) {
+   }
+
+   this._player = $(container).jPlayer({
+      wmode: 'window',
+      preload: 'none',
+      swfPath: '/static/js/',
+      supplied: 'mp3',
+      volume: 1,
+      solution: engine
+   });
+
+   if (typeof (params.onplay) === 'function') {
+      this._player.bind($.jPlayer.event.play, params.onplay);
+   }
+
+   if (typeof (params.onended) === 'function') {
+      this._player.bind($.jPlayer.event.ended, params.onended);
+   }
+};
+
+Player.prototype.play = function(mp3Url) {
+   if (this._player == undefined) {
+      console.error('Player in not initialized');
+      return;
+   }
+
+   this._player.jPlayer('setMedia', {
+      mp3: mp3Url
+   }).jPlayer('play');
+};
